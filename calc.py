07c47d829e8a5d56c136d7ec0a87a3b815eccc89
Shyam Patel <158061330+Shyam79Patel@users.noreply.github.com>
2025-09-04 09:20:46 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,3 @@
+from main import calculation
+
+print(calculation([0,1,2,3,4,5,6,7,8]))
---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def calculation(list1):
+    if len(list1) != 9:
+        raise ValueError("List Must Contain 9 Numbers")
+
+    matrix = np.array(list1).reshape(3,3)
+
+    calculate = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().item()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().item()],
+        'std dev': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().item()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().item()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().item()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+    }
+
+    return calculate
